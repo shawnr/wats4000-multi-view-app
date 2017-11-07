b8853970d381e5a70fab1cc87fcb87be3ee2bcc7
Shawn Rider <shawn@shawnrider.com>
2017-11-07 14:54:10 -0800

    Built code.
    Change-Id: Ida60f264989f96c4fcd06675f5d7c311edb47ab0

---
+++ src/components/Secret.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="secret">
+    <h1>{{ message }}</h1>
+    <ul>
+      <li><router-link v-bind:to="{ name: 'Home'}">Home</router-link></li>
+      <li><router-link to="/survey">Survey</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Secret',
+  data () {
+    return {
+      message: 'Always check your devtools!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.error {
+  border: 1px solid #aa0000;
+  padding: 1rem;
+  color: #aa0000;
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
